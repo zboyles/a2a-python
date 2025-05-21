c664c20a171e1db92ed3ab8286947bcb31bac63c
MEUNIER Laurent <laurent@breezyai.co>
2025-05-21 11:41:18 +0200

    feat: postgresql task store

--- src/a2a/server/tasks/__init__.py
+++ src/a2a/server/tasks/__init__.py
@@ -2,6 +2,7 @@
 
 from a2a.server.tasks.inmemory_push_notifier import InMemoryPushNotifier
 from a2a.server.tasks.inmemory_task_store import InMemoryTaskStore
+from a2a.server.tasks.postgresql_task_store import PostgreSQLTaskStore
 from a2a.server.tasks.push_notifier import PushNotifier
 from a2a.server.tasks.result_aggregator import ResultAggregator
 from a2a.server.tasks.task_manager import TaskManager
@@ -12,6 +13,7 @@ from a2a.server.tasks.task_updater import TaskUpdater
 __all__ = [
     'InMemoryPushNotifier',
     'InMemoryTaskStore',
+    'PostgreSQLTaskStore',
     'PushNotifier',
     'ResultAggregator',
     'TaskManager',
