18d1954122e532b7773048af49532e0cbd69df9f
Mateusz Czubak <mateusz@firebolt.io>
2025-05-23 15:39:57 +0200

    chore: remove unused `HttpApp` base class (#102)

--- src/a2a/server/apps/__init__.py
+++ src/a2a/server/apps/__init__.py
@@ -1,7 +1,6 @@
 """HTTP application components for the A2A server."""
 
-from a2a.server.apps.http_app import HttpApp
 from a2a.server.apps.starlette_app import A2AStarletteApplication
 
 
-__all__ = ['A2AStarletteApplication', 'HttpApp']
+__all__ = ['A2AStarletteApplication']
